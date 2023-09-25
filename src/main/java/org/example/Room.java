fe43955846e6eb075a0eb6fc6209e4b6b237925b
GustavSoederberg <guso0001@stud.kea.dk>
2023-09-25 11:46:51 +0200

    2nd commit added Rooms

--- src/main/java/org/example/Room.java
+++ src/main/java/org/example/Room.java
@@ -1,6 +1,63 @@
 package org.example;
 
 public class Room {
+    private String name;
+    private String description;
+    private Room north;
+    private Room south;
+    private Room east;
+    private Room west;
 
+    public Room(String name, String description){
+        this.name = name;
+        this.description = description;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
 }
