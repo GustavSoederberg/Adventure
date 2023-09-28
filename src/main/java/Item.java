f2d49860c727fdae56777ba714689ade9219e033
GustavSoederberg <guso0001@stud.kea.dk>
2023-09-28 12:52:51 +0200

    Added Item Class

---
+++ src/main/java/Item.java
@@ -0,0 +1,25 @@
+public class Item {
+    private String name;
+    private String description;
+
+    public Item(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
