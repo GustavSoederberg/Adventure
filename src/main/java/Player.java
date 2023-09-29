b71d8a9e48d51e340d9f6656a18029207f6ba156
GustavSoederberg <guso0001@stud.kea.dk>
2023-09-29 14:32:09 +0200

    Improved UI

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -109,7 +109,7 @@ public class Player {
     public Item findItem(String searchItem, ArrayList<Item> items) {
         Item itemResult;
         for (Item i : items) {
-            if (searchItem.equals(i.getName())) {
+            if (searchItem.equalsIgnoreCase(i.getName())) {
                 itemResult = i;
                 return itemResult;
             }
